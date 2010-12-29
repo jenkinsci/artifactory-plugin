2b713680c1f64e9dfa748d89f98cede61d3dddd7
jfrog <jfrog@dev.java.net>
2010-12-29 16:33:21 +0000

    timestamp should be stringed when put into env

--- src/main/java/org/jfrog/hudson/maven3/ArtifactoryMaven3Configurator.java
+++ src/main/java/org/jfrog/hudson/maven3/ArtifactoryMaven3Configurator.java
@@ -285,7 +285,7 @@ public class ArtifactoryMaven3Configurator extends BuildWrapper implements Deplo
         props.put(BuildInfoProperties.PROP_BUILD_STARTED,
                 new SimpleDateFormat(Build.STARTED_FORMAT).format(buildStartDate));
 
-        props.put(BuildInfoProperties.PROP_BUILD_TIMESTAMP, buildStartDate.getTime());
+        props.put(BuildInfoProperties.PROP_BUILD_TIMESTAMP, String.valueOf(buildStartDate.getTime()));
         props.put(ClientProperties.PROP_DEPLOY_PARAM_PROP_PREFIX + "build.timestamp",
                 String.valueOf(buildStartDate.getTime()));
 
