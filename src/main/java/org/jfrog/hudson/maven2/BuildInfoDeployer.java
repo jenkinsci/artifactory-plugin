4c5bcc4ba976640b5cd4e35628675e73832d19ca
yossis <yossis@jfrog.org>
2011-03-03 15:02:47 +0200

    Setting ci user

--- src/main/java/org/jfrog/hudson/maven2/BuildInfoDeployer.java
+++ src/main/java/org/jfrog/hudson/maven2/BuildInfoDeployer.java
@@ -177,7 +177,7 @@ public class BuildInfoDeployer {
                     .timestampDate(startedTimestamp.getTime())
                     .comment(release.getStagingComment())
                     .repository(release.getStagingRepositoryKey())
-                    .user(userCause).build());
+                    .ciUser(userCause).build());
         }
 
         Build buildInfo = infoBuilder.build();
