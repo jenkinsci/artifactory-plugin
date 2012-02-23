b0aaf9e08025111a7dd92b4d6c4c3df3f189547b
Shay Yaakov <shayy@jfrog.org>
2012-02-23 14:34:08 +0200

    HAP-133 - Support Perforce in release management * Added dependeny to build-info-vcs and refactored the PerforceCoordinator to it

--- src/main/java/org/jfrog/hudson/release/scm/perforce/PerforceManager.java
+++ src/main/java/org/jfrog/hudson/release/scm/perforce/PerforceManager.java
@@ -16,26 +16,19 @@
 
 package org.jfrog.hudson.release.scm.perforce;
 
+import com.perforce.p4java.core.IChangelist;
 import com.tek42.perforce.Depot;
-import com.tek42.perforce.PerforceException;
-import com.tek42.perforce.model.Label;
 import hudson.model.AbstractBuild;
 import hudson.model.TaskListener;
 import hudson.plugins.perforce.PerforceSCM;
 import hudson.plugins.perforce.PerforceTagAction;
+import org.jfrog.build.vcs.perforce.PerforceClient;
 import org.jfrog.hudson.action.ActionableHelper;
 import org.jfrog.hudson.release.scm.AbstractScmManager;
-import org.jfrog.hudson.release.scm.perforce.command.DeleteLabel;
-import org.jfrog.hudson.release.scm.perforce.command.Edit;
-import org.jfrog.hudson.release.scm.perforce.command.Revert;
-import org.jfrog.hudson.release.scm.perforce.command.Submit;
 
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.util.List;
-import java.util.logging.Logger;
 
 /**
  * Performs Perforce actions for the release management.
@@ -43,14 +36,60 @@ import java.util.logging.Logger;
  * @author Yossi Shaul
  */
 public class PerforceManager extends AbstractScmManager<PerforceSCM> {
-    private static Logger debuggingLogger = Logger.getLogger(PerforceManager.class.getName());
+
+    private PerforceClient perforce;
 
     public PerforceManager(AbstractBuild<?, ?> build, TaskListener buildListener) {
         super(build, buildListener);
     }
 
-    public void commitWorkingCopy(String commitMessage) throws IOException {
-        submit(commitMessage);
+    public void prepare() throws IOException {
+        PerforceClient.Builder builder = new PerforceClient.Builder();
+        Depot perforceDepot = getDepot();
+        String hostAddress = perforceDepot.getPort();
+        if (!hostAddress.contains(":")) {
+            hostAddress = "localhost:" + hostAddress;
+        }
+        builder.hostAddress(hostAddress).client(perforceDepot.getClient());
+        builder.username(perforceDepot.getUser()).password(perforceDepot.getPassword());
+        perforce = builder.build();
+    }
+
+    public void commitWorkingCopy(int changeListId, String commitMessage) throws IOException {
+        perforce.commitWorkingCopy(changeListId, commitMessage);
+    }
+
+    public void createTag(String label, String commitMessage, String changeListId) throws IOException {
+        perforce.createLabel(label, commitMessage, changeListId);
+    }
+
+    public void revertWorkingCopy(int changeListId) throws IOException {
+        perforce.revertWorkingCopy(changeListId);
+    }
+
+    public void deleteLabel(String tagUrl) throws IOException {
+        perforce.deleteLabel(tagUrl);
+    }
+
+    public void edit(int changeListId, File releaseVersion) throws IOException {
+        perforce.editFile(changeListId, releaseVersion);
+    }
+
+    /**
+     * Creates a new changelist and returns its id number
+     * @return The id of the newly created changelist
+     * @throws IOException In case of errors communicating with perforce server
+     */
+    public int createNewChangeList() throws IOException {
+        return perforce.createNewChangeList();
+    }
+
+    public void deleteChangeList(int changeListId) throws IOException {
+        perforce.deleteChangeList(changeListId);
+    }
+
+    public int getDefaultChangeListId() throws IOException {
+        return perforce.getDefaultChangeListId();
     }
 
     private Depot getDepot() {
@@ -64,76 +103,15 @@ public class PerforceManager extends AbstractScmManager<PerforceSCM> {
         }
     }
 
-    public void createTag(String tagUrl, String commitMessage) throws IOException {
-        PerforceTagAction perforceTagAction = ActionableHelper.getLatestAction(build, PerforceTagAction.class);
-        //perforceTagAction.tagBuild(tagUrl, commitMessage);
-        try {
-            Depot depot = getDepot();
-            Label label = new Label();
-            label.setName(tagUrl);
-            label.setDescription(commitMessage);
-            label.setOwner(perforceTagAction.getOwner());
-            label.setRevision(perforceTagAction.getChangeNumber() + "");
-            List<String> viewPairs = parseViewPairs(perforceTagAction);
-            //List<String> viewPairs = PerforceSCM.parseProjectPath(perforceTagAction.getView(), "workspace");
-            for (int i = 0; i < viewPairs.size(); i += 2) {
-                String depotPath = viewPairs.get(i);
-                label.addView(depotPath);
-            }
-            depot.getLabels().saveLabel(label);
-        } catch (PerforceException e) {
-            throw new IOException("Failed to revert changelist: " + e.getMessage());
-        }
+    public void commitWorkingCopy(String commitMessage) throws IOException, InterruptedException {
+        throw new UnsupportedOperationException("Use the overloaded method");
     }
 
-    private List<String> parseViewPairs(PerforceTagAction perforceTagAction) throws IOException {
-        try {
-            String p4Client = build.getEnvironment(buildListener).get("P4CLIENT");
-            Method method = PerforceSCM.class.getDeclaredMethod("parseProjectPath", String.class, String.class);
-            method.setAccessible(true);
-            return (List<String>) method.invoke(null, perforceTagAction.getView(), p4Client);
-        } catch (Exception e) {
-            throw new IOException("Failed to parse view pair: " + e.getMessage());
-        }
+    public void createTag(String tagUrl, String commitMessage) throws IOException, InterruptedException {
+        throw new UnsupportedOperationException("Use the overloaded method");
     }
 
     public String getRemoteUrl() {
         throw new UnsupportedOperationException("Remote URL not supported");
-    }
-
-    public void edit(File file) throws IOException {
-        Depot depot = getDepot();
-        try {
-            new Edit(depot, file).editFile();
-        } catch (PerforceException e) {
-            throw new IOException("Failed to edit file: " + e.getMessage());
-        }
-    }
-
-    private void submit(String message) throws IOException {
-        Depot depot = getDepot();
-        try {
-            new Submit(depot).submit(message);
-        } catch (PerforceException e) {
-            throw new IOException("Failed to submit changelist: " + e.getMessage());
-        }
-    }
-
-    public void revertWorkingCopy() throws IOException {
-        try {
-            Depot depot = getDepot();
-            new Revert(depot).revert();
-        } catch (PerforceException e) {
-            throw new IOException("Failed to revert changelist: " + e.getMessage());
-        }
-    }
-
-    public void deleteLabel(String labelName) throws IOException {
-        try {
-            Depot depot = getDepot();
-            new DeleteLabel(depot, labelName).deleteLabel();
-        } catch (PerforceException e) {
-            throw new IOException("Failed to delete label: " + e.getMessage());
-        }
     }
 }
