2fd92f9f470737de5dea10cc1d58d81e4c81afd7
yossis <yossis@jfrog.org>
2012-10-10 13:09:05 +0200

    HAP-343 - Using DAYS_OF_YEAR to align with build info code

--- src/main/java/org/jfrog/hudson/util/BuildRetentionFactory.java
+++ src/main/java/org/jfrog/hudson/util/BuildRetentionFactory.java
@@ -31,7 +31,7 @@ public class BuildRetentionFactory {
         }
         if (rotator.getDaysToKeep() > -1) {
             Calendar calendar = Calendar.getInstance();
-            calendar.add(Calendar.DAY_OF_MONTH, -rotator.getDaysToKeep());
+            calendar.add(Calendar.DAY_OF_YEAR, -rotator.getDaysToKeep());
             buildRetention.setMinimumBuildDate(new Date(calendar.getTimeInMillis()));
         }
         List<String> notToBeDeleted = ExtractorUtils.getBuildNumbersNotToBeDeleted(build);
