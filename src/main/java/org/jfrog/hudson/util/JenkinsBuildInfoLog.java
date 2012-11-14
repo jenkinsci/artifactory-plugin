fc5d5f83cb447b1af724f3aeb458aeedf9f475b0
Shay Yaakov <shayy@jfrog.com>
2012-11-14 10:19:08 +0200

    Using JUL to point logs to Jenkins general system log

--- src/main/java/org/jfrog/hudson/util/JenkinsBuildInfoLog.java
+++ src/main/java/org/jfrog/hudson/util/JenkinsBuildInfoLog.java
@@ -3,12 +3,17 @@ package org.jfrog.hudson.util;
 import hudson.model.BuildListener;
 import org.jfrog.build.api.util.Log;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Wrapper for Jenkins build logger, records log messages from BuildInfo
  *
  * @author Shay Yaakov
  */
 public class JenkinsBuildInfoLog implements Log {
+    private static final Logger logger = Logger.getLogger(JenkinsBuildInfoLog.class.getName());
+
     private BuildListener listener;
 
     public JenkinsBuildInfoLog(BuildListener listener) {
@@ -16,22 +21,26 @@ public class JenkinsBuildInfoLog implements Log {
     }
 
     public void debug(String message) {
-        listener.getLogger().println(message);
+        logger.finest(message);
     }
 
     public void info(String message) {
         listener.getLogger().println(message);
+        logger.info(message);
     }
 
     public void warn(String message) {
         listener.getLogger().println(message);
+        logger.warning(message);
     }
 
     public void error(String message) {
         listener.getLogger().println(message);
+        logger.severe(message);
     }
 
     public void error(String message, Throwable e) {
         listener.getLogger().println(message);
+        logger.log(Level.SEVERE, message, e);
     }
 }
