85091f23cb7aa38daa428551fe426ccb935032bc
liorha <liorha@jfrog.com>
2015-08-09 11:17:29 +0300

    HAP-616

--- src/main/java/org/jfrog/hudson/util/converters/DeployerResolverOverriderConverter.java
+++ src/main/java/org/jfrog/hudson/util/converters/DeployerResolverOverriderConverter.java
@@ -16,6 +16,7 @@
 
 package org.jfrog.hudson.util.converters;
 
+import com.google.common.collect.Lists;
 import com.thoughtworks.xstream.converters.UnmarshallingContext;
 import hudson.util.Scrambler;
 import hudson.util.XStream2;
@@ -24,16 +25,18 @@ import org.jfrog.hudson.DeployerOverrider;
 import org.jfrog.hudson.util.Credentials;
 
 import java.lang.reflect.Field;
+import java.util.List;
 
 /**
- * When upgrading from an older version, a user might have deployer credentials as local variables of a builder
- * configuration. This converter Will check for existing old deployer credentials and "move" them to a credentials
- * object instead, thus overriding the deployer credentials of the global config.
+ * General converter for BuildWrapper or Publisher
  *
  * @author Noam Y. Tenne
+ * @author Lior Hasson
  */
 public class DeployerResolverOverriderConverter<T extends DeployerOverrider>
         extends XStream2.PassthruConverter<T> {
+
+    List<String> converterErrors = Lists.newArrayList();
 
     public DeployerResolverOverriderConverter(XStream2 xstream) {
         super(xstream);
@@ -42,74 +45,79 @@ public class DeployerResolverOverriderConverter<T extends DeployerOverrider>
     @Override
     protected void callback(T overrider, UnmarshallingContext context) {
         Class<? extends DeployerOverrider> overriderClass = overrider.getClass();
+        overrideDeployerCredentials(overrider, overriderClass);
+        overrideResolverDetails(overrider, overriderClass);
 
-        try {
-            overrideDeployerCredentials(overrider, overriderClass);
-            overrideResolverDetails(overrider, overriderClass);
-        } catch (NoSuchFieldException e) {
-            throw new RuntimeException(getConversionErrorMessage(overrider), e);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(getConversionErrorMessage(overrider), e);
-        }
     }
 
     /**
-     * Convert any remaining local credential variables to a credentials object
+     * Convert any remaining local credential variables to a credentials object.
+     * When upgrading from an older version, a user might have deployer credentials as local variables of a builder
+     * configuration. This converter Will check for existing old deployer credentials and "move" them to a credentials
+     * object instead, thus overriding the deployer credentials of the global config.
      */
-    private void overrideDeployerCredentials(T deployerOverrider, Class<? extends DeployerOverrider> overriderClass)
-            throws NoSuchFieldException, IllegalAccessException {
-        Field oldUsernameField = overriderClass.getDeclaredField("username");
-        oldUsernameField.setAccessible(true);
-        Object oldUsernameValue = oldUsernameField.get(deployerOverrider);
+    private void overrideDeployerCredentials(T deployerOverrider, Class<? extends DeployerOverrider> overriderClass) {
+        try {
+            Field oldUsernameField = overriderClass.getDeclaredField("username");
+            oldUsernameField.setAccessible(true);
+            Object oldUsernameValue = oldUsernameField.get(deployerOverrider);
 
-        if (oldUsernameValue != null && StringUtils.isNotBlank((String) oldUsernameValue) &&
-                !deployerOverrider.isOverridingDefaultDeployer()) {
+            if (oldUsernameValue != null && StringUtils.isNotBlank((String) oldUsernameValue) &&
+                    !deployerOverrider.isOverridingDefaultDeployer()) {
 
-            String oldUsername = (String) oldUsernameValue;
-            String oldPassword = null;
-            Field oldPasswordField;
-            try {
-                oldPasswordField = overriderClass.getDeclaredField("password");
-            } catch (NoSuchFieldException e) {
-                oldPasswordField = overriderClass.getDeclaredField("scrambledPassword");
+                String oldUsername = (String) oldUsernameValue;
+                String oldPassword = null;
+                Field oldPasswordField;
+                try {
+                    oldPasswordField = overriderClass.getDeclaredField("password");
+                } catch (NoSuchFieldException e) {
+                    oldPasswordField = overriderClass.getDeclaredField("scrambledPassword");
+                }
+                oldPasswordField.setAccessible(true);
+                Object oldPasswordValue = oldPasswordField.get(deployerOverrider);
+                if ((oldPasswordValue != null) && StringUtils.isNotBlank(((String) oldPasswordValue))) {
+                    oldPassword = Scrambler.descramble((String) oldPasswordValue);
+                }
+
+                Field overridingCredentialsField = overriderClass.getDeclaredField("overridingDeployerCredentials");
+                overridingCredentialsField.setAccessible(true);
+                overridingCredentialsField.set(deployerOverrider, new Credentials(oldUsername, oldPassword));
             }
-            oldPasswordField.setAccessible(true);
-            Object oldPasswordValue = oldPasswordField.get(deployerOverrider);
-            if ((oldPasswordValue != null) && StringUtils.isNotBlank(((String) oldPasswordValue))) {
-                oldPassword = Scrambler.descramble((String) oldPasswordValue);
-            }
-
-            Field overridingCredentialsField = overriderClass.getDeclaredField("overridingDeployerCredentials");
-            overridingCredentialsField.setAccessible(true);
-            overridingCredentialsField.set(deployerOverrider, new Credentials(oldUsername, oldPassword));
+        }
+        catch (NoSuchFieldException e) {
+            converterErrors.add(getConversionErrorMessage(deployerOverrider, e));
+        } catch (IllegalAccessException e) {
+            converterErrors.add(getConversionErrorMessage(deployerOverrider, e));
         }
     }
 
     /**
      * Convert the (ServerDetails)details to (ServerDetails)resolverDetails if it doesn't exists already
      */
-    private void overrideResolverDetails(T resolverOverrider, Class<? extends DeployerOverrider> overriderClass)
-            throws NoSuchFieldException, IllegalAccessException {
-        Field resolverDetailsField = overriderClass.getDeclaredField("resolverDetails");
-        resolverDetailsField.setAccessible(true);
-        Object resolverDetails = resolverDetailsField.get(resolverOverrider);
+    private void overrideResolverDetails(T resolverOverrider, Class<? extends DeployerOverrider> overriderClass) {
 
-        if (resolverDetails == null) {
-            Field deployerDetailsField = overriderClass.getDeclaredField("details");
-            deployerDetailsField.setAccessible(true);
-            Object deployerDetails = deployerDetailsField.get(overriderClass);
+        try{
+            Field resolverDetailsField = overriderClass.getDeclaredField("resolverDetails");
+            resolverDetailsField.setAccessible(true);
+            Object resolverDetails = resolverDetailsField.get(resolverOverrider);
 
-//                Class<? extends ServerDetails> dClass = deployerDetails.getClass();
-//                Field artifactoryNameField = dClass.getDeclaredField("artifactoryName");
-//                artifactoryNameField.setAccessible(true);
-//                Object deployArtifactoryName = artifactoryNameField.get(deployerDetails);
+            if (resolverDetails == null) {
+                Field deployerDetailsField = overriderClass.getDeclaredField("details");
+                deployerDetailsField.setAccessible(true);
+                Object deployerDetails = deployerDetailsField.get(resolverOverrider);
 
-            resolverDetailsField.set(resolverOverrider, deployerDetails);
+                resolverDetailsField.set(resolverOverrider, deployerDetails);
+            }
+        }
+        catch (NoSuchFieldException e) {
+            converterErrors.add(getConversionErrorMessage(resolverOverrider, e));
+        } catch (IllegalAccessException e) {
+            converterErrors.add(getConversionErrorMessage(resolverOverrider, e));
         }
     }
 
-    private String getConversionErrorMessage(T deployerOverrider) {
-        return String.format("Could not convert the class '%s' to use the new overriding deployer credential storage " +
-                "format.", deployerOverrider.getClass().getName());
+    private String getConversionErrorMessage(T deployerOverrider, Exception e) {
+        return String.format("Could not convert the class '%s' to use the new overriding" +
+                "format. Cause: %s", deployerOverrider.getClass().getName(), e.getCause());
     }
 }
--- src/main/java/org/jfrog/hudson/util/converters/ServerDetailsConverter.java
+++ src/main/java/org/jfrog/hudson/util/converters/ServerDetailsConverter.java
@@ -1,5 +1,6 @@
 package org.jfrog.hudson.util.converters;
 
+import com.google.common.collect.Lists;
 import com.thoughtworks.xstream.converters.UnmarshallingContext;
 import hudson.util.XStream2;
 import org.apache.commons.lang.StringUtils;
@@ -8,6 +9,7 @@ import org.jfrog.hudson.ServerDetails;
 
 import java.lang.reflect.Field;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -15,6 +17,7 @@ import java.util.Map;
  * @author Lior Hasson
  */
 public class ServerDetailsConverter extends XStream2.PassthruConverter<ServerDetails> {
+    List<String> converterErrors = Lists.newArrayList();
     // mapping of the old ServerDetails field to the corresponding new field
     private static final Map<String, String> newToOldFields;
 
@@ -29,58 +32,68 @@ public class ServerDetailsConverter extends XStream2.PassthruConverter<ServerDet
         super(xstream);
     }
 
-    public void convertToReleaseAndSnapshotRepository(ServerDetails server) throws NoSuchFieldException, IllegalAccessException {
+    public void convertToReleaseAndSnapshotRepository(ServerDetails server) {
         Class<? extends ServerDetails> overrideClass = server.getClass();
 
-        Field oldReleaseRepositoryField = overrideClass.getDeclaredField("downloadRepositoryKey");
-        oldReleaseRepositoryField.setAccessible(true);
-        Object oldReleaseRepositoryValue = oldReleaseRepositoryField.get(server);
+        try{
+            Field oldReleaseRepositoryField = overrideClass.getDeclaredField("downloadRepositoryKey");
+            oldReleaseRepositoryField.setAccessible(true);
+            Object oldReleaseRepositoryValue = oldReleaseRepositoryField.get(server);
 
-        if (oldReleaseRepositoryValue != null && StringUtils.isNotBlank((String) oldReleaseRepositoryValue)) {
-            Field newReleaseRepositoryField = overrideClass.getDeclaredField("downloadReleaseRepositoryKey");
-            newReleaseRepositoryField.setAccessible(true);
-            newReleaseRepositoryField.set(server, oldReleaseRepositoryValue);
+            if (oldReleaseRepositoryValue != null && StringUtils.isNotBlank((String) oldReleaseRepositoryValue)) {
+                Field newReleaseRepositoryField = overrideClass.getDeclaredField("downloadReleaseRepositoryKey");
+                newReleaseRepositoryField.setAccessible(true);
+                newReleaseRepositoryField.set(server, oldReleaseRepositoryValue);
 
-            Field newSnapshotRepositoryField = overrideClass.getDeclaredField("downloadSnapshotRepositoryKey");
-            newSnapshotRepositoryField.setAccessible(true);
-            newSnapshotRepositoryField.set(server, oldReleaseRepositoryValue);
+                Field newSnapshotRepositoryField = overrideClass.getDeclaredField("downloadSnapshotRepositoryKey");
+                newSnapshotRepositoryField.setAccessible(true);
+                newSnapshotRepositoryField.set(server, oldReleaseRepositoryValue);
+            }
+        } catch (NoSuchFieldException e) {
+            converterErrors.add(getConversionErrorMessage(server, e));
+        } catch (IllegalAccessException e) {
+            converterErrors.add(getConversionErrorMessage(server, e));
         }
     }
 
-    public void convertToDynamicReposSelection(ServerDetails server) throws NoSuchFieldException, IllegalAccessException {
+    public void convertToDynamicReposSelection(ServerDetails server) {
         Class<? extends ServerDetails> overrideClass = server.getClass();
         for (Map.Entry<String, String> e : newToOldFields.entrySet()) {
             setNewReposFieldFromOld(server, overrideClass, e.getKey(), e.getValue());
         }
     }
 
-    private void setNewReposFieldFromOld(Object reflectedObject, Class classToChange, String oldFieldName,
-                                         String newFieldName) throws NoSuchFieldException, IllegalAccessException {
-        Field oldField = classToChange.getDeclaredField(oldFieldName);
-        oldField.setAccessible(true);
-        String oldValue = (String) oldField.get(reflectedObject);
-        if (StringUtils.isNotBlank(oldValue)) {
-            Field newField = classToChange.getDeclaredField(newFieldName);
-            RepositoryConf newValue = new RepositoryConf(oldValue, oldValue, false);
-            newField.setAccessible(true);
-            newField.set(reflectedObject, newValue);
+    private void setNewReposFieldFromOld(ServerDetails reflectedObject, Class classToChange, String oldFieldName,
+                                         String newFieldName)  {
+        try{
+            Field oldField = classToChange.getDeclaredField(oldFieldName);
+            oldField.setAccessible(true);
+            String oldValue = (String) oldField.get(reflectedObject);
+            if (StringUtils.isNotBlank(oldValue)) {
+                Field newField = classToChange.getDeclaredField(newFieldName);
+                RepositoryConf newValue = new RepositoryConf(oldValue, oldValue, false);
+                newField.setAccessible(true);
+                newField.set(reflectedObject, newValue);
+            }
+        } catch (NoSuchFieldException e) {
+            converterErrors.add(getConversionErrorMessage(reflectedObject, e));
+        } catch (IllegalAccessException e) {
+            converterErrors.add(getConversionErrorMessage(reflectedObject, e));
         }
     }
 
     @Override
     protected void callback(ServerDetails server, UnmarshallingContext context) {
-        try {
-            convertToReleaseAndSnapshotRepository(server);
-            convertToDynamicReposSelection(server);
-        } catch (NoSuchFieldException e) {
-            throw new RuntimeException(getConversionErrorMessage(server), e);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(getConversionErrorMessage(server), e);
+        convertToReleaseAndSnapshotRepository(server);
+        convertToDynamicReposSelection(server);
+
+        if(!converterErrors.isEmpty()){
+            throw new RuntimeException(converterErrors.toString());
         }
     }
 
-    private String getConversionErrorMessage(ServerDetails serverDetails) {
-        return String.format("Could not convert the class '%s' to use the new overriding Resolve repositories."
-                , serverDetails.getClass().getName());
+    private String getConversionErrorMessage(ServerDetails serverDetails, Exception e) {
+        return String.format("Could not convert the class '%s' to use the new overriding Resolve repositories." +
+                " Cause: %s", serverDetails.getClass().getName(), e.getCause());
     }
 }
