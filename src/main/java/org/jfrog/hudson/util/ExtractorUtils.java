10492cab41e8e4cc646caa35f9db42405777b993
yahavi <yahavizhak@gmail.com>
2018-12-30 20:14:00 +0200

    Revert "ExtractorUtils: make it work with non-default workspace name"
    This reverts commit 6121cd24296ab8229eb63f77eeec3ec4767e65ea.

--- src/main/java/org/jfrog/hudson/util/ExtractorUtils.java
+++ src/main/java/org/jfrog/hudson/util/ExtractorUtils.java
@@ -581,19 +581,13 @@ public class ExtractorUtils {
     }
 
     /**
-     * The token that combines the project name and unique number to create unique workspace directory.
-     */
-    private static final String WORKSPACELIST = System.getProperty("hudson.slaves.WorkspaceList");
-
-    /**
      * Create a temporary directory under a given workspace
      * @param launcher
      * @param ws
      * @throws Exception
      */
     public static FilePath createAndGetTempDir(hudson.Launcher launcher, FilePath ws) throws Exception {
-        final String WORKSPACELIST = System.getProperty("hudson.slaves.WorkspaceList");
-        final FilePath tempDirPath = new FilePath(ws.getParent(), ws.getName() + ((WORKSPACELIST != null) ? WORKSPACELIST : "@") + "tmp");
+        final FilePath tempDirPath = new FilePath(ws.getParent(), ws.getName() + "@tmp");
         launcher.getChannel().call(new MasterToSlaveCallable<Boolean, IOException>() {
             public Boolean call() {
                 File tempDirFile = new File(tempDirPath.getRemote());
