8de98e32cb6d80450aeac547548ad395f57fb92f
Alexei Vainshtein <AlexeiVainshtein@users.noreply.github.com>
2019-07-17 11:10:26 +0300

    HAP-1207 - Support multiple paths in Artifactory trigger (#160)
    * HAP-1207 - Support multiple paths in Artifactory trigger

--- src/main/java/org/jfrog/hudson/trigger/ArtifactoryTrigger.java
+++ src/main/java/org/jfrog/hudson/trigger/ArtifactoryTrigger.java
@@ -7,6 +7,7 @@ import hudson.model.*;
 import hudson.triggers.Trigger;
 import hudson.triggers.TriggerDescriptor;
 import jenkins.model.Jenkins;
+import org.apache.commons.lang3.StringUtils;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jfrog.build.api.util.NullLog;
 import org.jfrog.build.client.ItemLastModified;
@@ -28,14 +29,14 @@ import java.util.logging.Logger;
 public class ArtifactoryTrigger extends Trigger {
     private static final Logger logger = Logger.getLogger(JenkinsBuildInfoLog.class.getName());
 
-    private String path;
+    private String paths;
     private ServerDetails details;
     private long lastModified = System.currentTimeMillis();
 
     @DataBoundConstructor
-    public ArtifactoryTrigger(String path, String spec, ServerDetails details) throws ANTLRException {
+    public ArtifactoryTrigger(String paths, String spec, ServerDetails details) throws ANTLRException {
         super(spec);
-        this.path = path;
+        this.paths = paths;
         this.details = details;
     }
 
@@ -51,39 +52,43 @@ public class ArtifactoryTrigger extends Trigger {
             return;
         }
 
-        try (ArtifactoryBuildInfoClient client = server.createArtifactoryClient(
-                server.getDeployerCredentialsConfig().provideUsername(job),
-                server.getDeployerCredentialsConfig().providePassword(job),
-                server.createProxyConfiguration(Jenkins.getInstance().proxy),
-                new NullLog())) {
-            ItemLastModified itemLastModified = client.getItemLastModified(path);
-            long responseLastModified = itemLastModified.getLastModified();
-            if (responseLastModified > lastModified) {
-                this.lastModified = responseLastModified;
-                if (job instanceof Project) {
-                    AbstractProject<?, ?> project = ((Project) job).getRootProject();
-                    saveAndSchedule(itemLastModified, project);
-                    return;
-                }
+        String[] paths = this.paths.split(";");
+        for (String path : paths) {
+            try (ArtifactoryBuildInfoClient client = server.createArtifactoryClient(
+                    server.getDeployerCredentialsConfig().provideUsername(job),
+                    server.getDeployerCredentialsConfig().providePassword(job),
+                    server.createProxyConfiguration(Jenkins.getInstance().proxy),
+                    new NullLog())) {
+                ItemLastModified itemLastModified = client.getItemLastModified(StringUtils.trimToEmpty(path));
+                long responseLastModified = itemLastModified.getLastModified();
+                if (responseLastModified > lastModified) {
+                    this.lastModified = responseLastModified;
+                    if (job instanceof Project) {
+                        AbstractProject<?, ?> project = ((Project) job).getRootProject();
+                        saveAndSchedule(itemLastModified, project);
+                        return;
+                    }
 
-                if (job instanceof MavenModuleSet) {
-                    AbstractProject project = ((MavenModuleSet)job).getRootProject();
-                    saveAndSchedule(itemLastModified, project);
-                    return;
-                }
+                    if (job instanceof MavenModuleSet) {
+                        AbstractProject project = ((MavenModuleSet) job).getRootProject();
+                        saveAndSchedule(itemLastModified, project);
+                        return;
+                    }
 
-                if (job instanceof WorkflowJob) {
-                    WorkflowJob project = (WorkflowJob) job;
-                    logger.fine("Updating " + job.getName());
-                    project.save();
-                    project.scheduleBuild(new ArtifactoryCause(itemLastModified.getUri()));
+                    if (job instanceof WorkflowJob) {
+                        WorkflowJob project = (WorkflowJob) job;
+                        logger.fine("Updating " + job.getName());
+                        project.save();
+                        project.scheduleBuild(new ArtifactoryCause(itemLastModified.getUri()));
+                        return;
+                    }
+                } else {
+                    logger.fine(String.format("Artifactory trigger did not trigger job %s, since last modified time: %d is earlier or equal than %d for path %s", job.getName(), responseLastModified, lastModified, path));
                 }
-            } else {
-                logger.fine(String.format("Artifactory trigger did not trigger job %s, since last modified time: %d is earlier or equal than %d for path %s", job.getName(), responseLastModified, lastModified, path));
+            } catch (IOException | ParseException e) {
+                logger.severe("Received an error: " + e.getMessage());
+                logger.fine("Received an error: " + e);
             }
-        } catch (IOException | ParseException e) {
-            logger.severe("Received an error: " + e.getMessage());
-            logger.fine("Received an error: " + e);
         }
     }
 
@@ -101,8 +106,8 @@ public class ArtifactoryTrigger extends Trigger {
         super.stop();
     }
 
-    public String getPath() {
-        return path;
+    public String getPaths() {
+        return paths;
     }
 
     public ServerDetails getDetails() {
